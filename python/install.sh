3f997cdd4b6f67a845e2452ac6267c86f4cacdc9
Lucas <lucas@lucas-ts.com>
2013-10-28 18:32:50 -0200

    install pyenv

--- python/install.sh
+++ python/install.sh
@@ -1,1 +1,8 @@
-easy_install SpoofMAC
+#!/bin/sh
+
+if test ! $(which pyenv)
+then
+  echo "  Installing rbenv for you."
+  brew install pyenv > /tmp/pyenv-install.log
+  brew install pyenv-virtualenv > /tmp/pyenv-virtualenv-install.log
+fi
